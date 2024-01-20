f15845917ec9876acea07b3dfcebb3e4b6c86bdf
Zdenek Havlin <wdolek@hotmail.com>
2024-01-20 22:48:54 +0100

    Make `ListenToSecretsManagerActivitySource` more convenient, set `Sample` and require callbacks for start/stop activities

--- W4k.Extensions.Configuration.Aws.SecretsManager/Diagnostics/ActivityListenerExtensions.cs
+++ W4k.Extensions.Configuration.Aws.SecretsManager/Diagnostics/ActivityListenerExtensions.cs
@@ -10,11 +10,20 @@ public static class ActivityListenerExtensions
     /// <summary>
     /// Configure <see cref="ActivityListener"/> to listen to <see cref="ActivityDescriptors.ActivitySourceName"/> activity source.
     /// </summary>
-    /// <param name="listener">Activity listener which should listen to <see cref="ActivityDescriptors.ActivitySourceName"/> activity source.</param>
+    /// <param name="listener">Activity listener which should listen to &quot;<see cref="ActivityDescriptors.ActivitySourceName"/>&quot; activity source.</param>
+    /// <param name="onStart">Callback invoked when activity is started.</param>
+    /// <param name="onStop">Callback invoked when activity is stopped.</param>
     /// <returns>Configured activity listener.</returns>
-    public static ActivityListener ListenToSecretsManagerActivitySource(this ActivityListener listener)
+    public static ActivityListener ListenToSecretsManagerActivitySource(
+        this ActivityListener listener,
+        Action<Activity> onStart,
+        Action<Activity> onStop)
     {
+        listener.ActivityStarted = onStart;
+        listener.ActivityStopped = onStop;
         listener.ShouldListenTo = activitySource => activitySource.Name == ActivityDescriptors.ActivitySourceName;
+        listener.Sample = (ref ActivityCreationOptions<ActivityContext> options) => ActivitySamplingResult.AllData;
+
         return listener;
     }
 }
