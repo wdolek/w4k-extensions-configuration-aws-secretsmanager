8d9796e0491b9a20da5b181df4df9e3c92ac2c47
Zdenek Havlin <wdolek@hotmail.com>
2025-01-25 13:12:13 +0100

    Introduce configuration source builder for convenient chainable configuration

--- src/W4k.Extensions.Configuration.Aws.SecretsManager/SecretsManagerConfigurationSource.cs
+++ src/W4k.Extensions.Configuration.Aws.SecretsManager/SecretsManagerConfigurationSource.cs
@@ -10,7 +10,7 @@ namespace W4k.Extensions.Configuration.Aws.SecretsManager;
 /// AWS Secrets Manager configuration source.
 /// </summary>
 [SuppressMessage("ReSharper", "RedundantNullableFlowAttribute", Justification = "Consumers of library may not have nullable reference types enabled.")]
-public class SecretsManagerConfigurationSource : IConfigurationSource
+public sealed class SecretsManagerConfigurationSource : IConfigurationSource
 {
     private TimeSpan _timeout = TimeSpan.FromSeconds(30);
 
@@ -108,6 +108,225 @@ public class SecretsManagerConfigurationSource : IConfigurationSource
         LoggerFactory ??= NullLoggerFactory.Instance;
 
         return new SecretsManagerConfigurationProvider(this);
+    }
+}
+
+/// <summary>
+/// Builder for <see cref="SecretsManagerConfigurationSource"/>.
+/// </summary>
+public sealed class SecretsManagerConfigurationBuilder
+{
+    private readonly SecretsManagerConfigurationSource _source;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SecretsManagerConfigurationBuilder"/> class.
+    /// </summary>
+    /// <param name="secretName">Secret name (or its complete ARN) to fetch.</param>
+    public SecretsManagerConfigurationBuilder(string secretName)
+    {
+        _source = new SecretsManagerConfigurationSource { SecretName = secretName };
+    }
+
+    /// <summary>
+    /// Sets <see cref="IAmazonSecretsManager"/> to the configuration source.
+    /// </summary>
+    /// <param name="secretsManager">AWS Secrets Manager client.</param>
+    /// <returns>Current builder instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="secretsManager"/> is <see langword="null"/>.</exception>
+    public SecretsManagerConfigurationBuilder WithSecretsManager(IAmazonSecretsManager secretsManager)
+    {
+        ArgumentNullException.ThrowIfNull(secretsManager);
+        _source.SecretsManager = secretsManager;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Sets secret version to fetch.
+    /// </summary>
+    /// <remarks>
+    /// When <see langword="null"/> is provided, latest version of secret is fetched.
+    /// </remarks>
+    /// <param name="versionId">Version ID.</param>
+    /// <param name="versionStage">Version stage name.</param>
+    /// <returns>Current builder instance.</returns>
+    public SecretsManagerConfigurationBuilder WithVersion(string? versionId, string? versionStage)
+    {
+        _source.Version = new SecretVersion
+        {
+            VersionId = versionId,
+            VersionStage = versionStage,
+        };
+
+        return this;
+    }
+
+    /// <summary>
+    /// Sets custom <see cref="ISecretProcessor"/> as secret processor.
+    /// </summary>
+    /// <param name="processor">Secret processor.</param>
+    /// <returns>Current builder instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="processor"/> is <see langword="null"/>.</exception>
+    public SecretsManagerConfigurationBuilder WithProcessor(ISecretProcessor processor)
+    {
+        ArgumentNullException.ThrowIfNull(processor);
+        _source.Processor = processor;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Sets <see cref="SecretsProcessor.Json"/> as secret processor.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="SecretsProcessor.Json"/> is used by default. Use this method to explicitly set it or overwrite another processor set.
+    /// </remarks>
+    /// <returns>Current builder instance.</returns>
+    public SecretsManagerConfigurationBuilder WithJsonProcessor()
+    {
+        _source.Processor = SecretsProcessor.Json;
+        return this;
+    }
+
+    /// <summary>
+    /// Clears all key transformers.
+    /// </summary>
+    /// <returns>Current builder instance.</returns>
+    public SecretsManagerConfigurationBuilder ClearKeyTransformers()
+    {
+        _source.KeyTransformers.Clear();
+        return this;
+    }
+
+    /// <summary>
+    /// Adds <see cref="IConfigurationKeyTransformer"/> to collection of key transformers.
+    /// </summary>
+    /// <remarks>
+    /// Transformers are applied in the order they are added to the list.
+    /// </remarks>
+    /// <param name="transformer">Key transformer.</param>
+    /// <returns>Current builder instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="transformer"/> is <see langword="null"/>.</exception>
+    public SecretsManagerConfigurationBuilder AddKeyTransformer(IConfigurationKeyTransformer transformer)
+    {
+        ArgumentNullException.ThrowIfNull(transformer);
+        _source.KeyTransformers.Add(transformer);
+
+        return this;
+    }
+
+    /// <summary>
+    /// Sets configuration key prefix.
+    /// </summary>
+    /// <param name="configurationKeyPrefix">Configuration key prefix.</param>
+    /// <returns>Current builder instance.</returns>
+    public SecretsManagerConfigurationBuilder WithConfigurationKeyPrefix(string configurationKeyPrefix)
+    {
+        _source.ConfigurationKeyPrefix = configurationKeyPrefix;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets <see cref="IConfigurationWatcher"/> to watch for configuration changes.
+    /// </summary>
+    /// <param name="configurationWatcher">Configuration watcher.</param>
+    /// <returns>Current builder instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="configurationWatcher"/> is <see langword="null"/>.</exception>
+    public SecretsManagerConfigurationBuilder WithConfigurationWatcher(IConfigurationWatcher configurationWatcher)
+    {
+        ArgumentNullException.ThrowIfNull(configurationWatcher);
+        _source.ConfigurationWatcher = configurationWatcher;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Sets <see cref="SecretsManagerPollingWatcher"/> to periodically watch for configuration changes.
+    /// </summary>
+    /// <param name="pollingInterval">Polling interval.</param>
+    /// <returns>Current builder instance.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="pollingInterval"/> is less than or equal to <see cref="TimeSpan.Zero"/>.</exception>
+    public SecretsManagerConfigurationBuilder WithPollingWatcher(TimeSpan pollingInterval)
+    {
+        _source.ConfigurationWatcher = new SecretsManagerPollingWatcher(pollingInterval);
+        return this;
+    }
+
+    /// <summary>
+    /// Sets <see cref="SecretsManagerPollingWatcher"/> to periodically watch for configuration changes.
+    /// </summary>
+    /// <param name="pollingInterval">Polling interval.</param>
+    /// <param name="timeProvider">Time provider.</param>
+    /// <returns>Current builder instance.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="pollingInterval"/> is less or equal to <see cref="TimeSpan.Zero"/>.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="timeProvider"/> is <see langword="null"/>.</exception>
+    public SecretsManagerConfigurationBuilder WithPollingWatcher(TimeSpan pollingInterval, TimeProvider timeProvider)
+    {
+        _source.ConfigurationWatcher = new SecretsManagerPollingWatcher(pollingInterval, timeProvider);
+        return this;
+    }
+
+    /// <summary>
+    /// Sets timeout for the secret fetch operation.
+    /// </summary>
+    /// <param name="timeout">Timeout.</param>
+    /// <returns>Current builder instance.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="timeout"/> is less than or equal to <see cref="TimeSpan.Zero"/>.</exception>
+    public SecretsManagerConfigurationBuilder WithTimeout(TimeSpan timeout)
+    {
+        _source.Timeout = timeout;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets error handler for exception thrown during loading secret.
+    /// </summary>
+    /// <param name="action">On load error callback.</param>
+    /// <returns>Current builder instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="action"/> is <see langword="null"/>.</exception>
+    public SecretsManagerConfigurationBuilder OnLoadException(Action<SecretsManagerExceptionContext> action)
+    {
+        ArgumentNullException.ThrowIfNull(action);
+        _source.OnLoadException = action;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Sets error handler for exception thrown during reloading secret.
+    /// </summary>
+    /// <param name="action">On reload error callback.</param>
+    /// <returns>Current builder instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="action"/> is <see langword="null"/>.</exception>
+    public SecretsManagerConfigurationBuilder OnReloadException(Action<SecretsManagerExceptionContext> action)
+    {
+        ArgumentNullException.ThrowIfNull(action);
+        _source.OnReloadException = action;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Sets <see cref="ILoggerFactory"/>.
+    /// </summary>
+    /// <param name="loggerFactory">Logger factory.</param>
+    /// <returns>Current builder instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="loggerFactory"/> is <see langword="null"/>.</exception>
+    public SecretsManagerConfigurationBuilder WithLoggerFactory(ILoggerFactory loggerFactory)
+    {
+        ArgumentNullException.ThrowIfNull(loggerFactory);
+        _source.LoggerFactory = loggerFactory;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Builds <see cref="SecretsManagerConfigurationProvider"/>.
+    /// </summary>
+    /// <returns>Configuration source.</returns>
+    public SecretsManagerConfigurationSource Build()
+    {
+        return _source;
     }
 }
 
